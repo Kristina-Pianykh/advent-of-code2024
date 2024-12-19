c1034cd25696b061b3f0e2157547323da4aae60b
Kristina Pianykh <kristinavrnrus@gmail.com>
2024-12-19 23:46:36 +0100

    part 2 wip

--- 15/warehouse.go
+++ 15/warehouse.go
@@ -9,6 +9,7 @@ import (
 type Coordinate struct {
 	x int
 	y int
+	v byte
 }
 
 func (c Coordinate) string() string {
@@ -30,74 +31,49 @@ func main() {
 		}
 		fmt.Println()
 	}
+	fmt.Printf("robot start pos: %s\n", w.robotPos.string())
 
-	w.write(00)
-	for _, move := range *w.moves {
+	w.write("00", (*w.moves)[0])
+	for i, move := range *w.moves {
 		w.update(move)
-		// w.write(i)
+		if i == len(*w.moves)-1 {
+			w.write(fmt.Sprintf("%d", i), '0')
+		} else {
+			w.write(fmt.Sprintf("%d", i), (*w.moves)[i+1])
+		}
+		fmt.Printf("updated robot position: %s\n", w.robotPos.string())
 	}
-	fmt.Printf("res: %d\n", w.calcGps())
+	// 1579161 too low
+	fmt.Printf("res: %d\n", w.calcGps2())
 	// fmt.Printf("%s\n", string(*moves))
 	// fmt.Printf("num of moves: %d\n", len(*moves))
 }
 
-// if has crate ahead, check if can push
-func (w *Warehouse) canPush(move byte) (int, bool) {
+func (w *Warehouse) canPushHorizontally(move byte) (int, bool) {
 	switch move {
 	case '<':
 		if (*w.grid)[w.robotPos.y][w.robotPos.x-1] == ']' {
 			i := 1
 			for {
-				if (*w.grid)[w.robotPos.y][(w.robotPos.x-i)*2] == '#' {
+				if (*w.grid)[w.robotPos.y][w.robotPos.x-i*2-1] == '#' {
 					return 0, false
 				}
-				if (*w.grid)[w.robotPos.y][(w.robotPos.x-i)*2] == '.' {
-					i--
+				if (*w.grid)[w.robotPos.y][w.robotPos.x-i*2-1] == '.' {
 					break
 				}
 				i++
 			}
+			// fmt.Printf("move < crates for cells: %d\n", i)
 			return i, true
 		}
 	case '>':
-		if (*w.grid)[w.robotPos.y][(w.robotPos.x+1)*2] == '[' {
+		if (*w.grid)[w.robotPos.y][w.robotPos.x+1] == '[' {
 			i := 1
 			for {
-				if (*w.grid)[w.robotPos.y][(w.robotPos.x+i)*2] == '#' {
+				if (*w.grid)[w.robotPos.y][w.robotPos.x+i*2+1] == '#' {
 					return 0, false
 				}
-				if (*w.grid)[w.robotPos.y][(w.robotPos.x+i)*2] == '.' {
-					i--
-					break
-				}
-				i++
-			}
-			return i, true
-		}
-	case '^':
-		if (*w.grid)[w.robotPos.y-1][w.robotPos.x] == ']' || (*w.grid)[w.robotPos.y-1][w.robotPos.x] == '[' {
-			i := 1
-			for {
-				if (*w.grid)[w.robotPos.y-i][w.robotPos.x] == '#' {
-					return 0, false
-				}
-				if (*w.grid)[w.robotPos.y-i][w.robotPos.x] == '.' {
-					i--
-					break
-				}
-				i++
-			}
-			return i, true
-		}
-	case 'v':
-		if (*w.grid)[w.robotPos.y+1][w.robotPos.x] == ']' || (*w.grid)[w.robotPos.y+1][w.robotPos.x] == '[' {
-			i := 1
-			for {
-				if (*w.grid)[w.robotPos.y+i][w.robotPos.x] == '#' {
-					return 0, false
-				}
-				if (*w.grid)[w.robotPos.y+i][w.robotPos.x] == '.' {
-					i--
+				if (*w.grid)[w.robotPos.y][w.robotPos.x+i*2+1] == '.' {
 					break
 				}
 				i++
@@ -110,6 +86,66 @@ func (w *Warehouse) canPush(move byte) (int, bool) {
 	return 0, false
 }
 
+// if has crate ahead, check if can push
+func (w *Warehouse) canPushVertically(move byte) ([]Coordinate, bool) {
+	var collectNodes func(Coordinate, byte) bool
+	nodes := []Coordinate{}
+
+	collectNodes = func(c Coordinate, move byte) bool {
+		c.v = (*w.grid)[c.y][c.x]
+		fmt.Printf("c: %s\n", c.string())
+		switch c.v {
+		case '.':
+			return true
+		case '#':
+			return false
+		case ']':
+			nodes = append(nodes, c)
+			closing := Coordinate{x: c.x - 1, y: c.y, v: '['}
+			nodes = append(nodes, closing)
+			if move == '^' {
+				return collectNodes(Coordinate{x: c.x, y: c.y - 1}, '^') && collectNodes(Coordinate{x: c.x - 1, y: c.y - 1}, '^')
+			} else if move == 'v' {
+				return collectNodes(Coordinate{x: c.x, y: c.y + 1}, 'v') && collectNodes(Coordinate{x: c.x - 1, y: c.y + 1}, 'v')
+			}
+		case '[':
+			nodes = append(nodes, c)
+			closing := Coordinate{x: c.x + 1, y: c.y, v: ']'}
+			nodes = append(nodes, closing)
+			if move == '^' {
+				return collectNodes(Coordinate{x: c.x, y: c.y - 1}, '^') && collectNodes(Coordinate{x: c.x + 1, y: c.y - 1}, '^')
+			} else if move == 'v' {
+				fmt.Printf("continue with going v\n")
+				return collectNodes(Coordinate{x: c.x, y: c.y + 1}, 'v') && collectNodes(Coordinate{x: c.x + 1, y: c.y + 1}, 'v')
+			}
+		default:
+			panic(fmt.Sprintf("unrecognized character: %c\n", c.v))
+		}
+		return false
+	}
+
+	switch move {
+	case '^':
+		if collectNodes(Coordinate{x: w.robotPos.x, y: w.robotPos.y - 1}, '^') {
+			for _, n := range nodes {
+				fmt.Printf("%s\n", n.string())
+			}
+			return nodes, true
+		}
+	case 'v':
+		fmt.Printf("going v\n")
+		if collectNodes(Coordinate{x: w.robotPos.x, y: w.robotPos.y + 1}, 'v') {
+			for _, n := range nodes {
+				fmt.Printf("%s\n", n.string())
+			}
+			return nodes, true
+		}
+	default:
+		panic("unrecognized move character\n")
+	}
+	return nil, false
+}
+
 func (w *Warehouse) updateRobotPos(xInc, yInc int) {
 	(*w.grid)[w.robotPos.y][w.robotPos.x] = '.'
 	w.robotPos.x += xInc
@@ -117,7 +153,21 @@ func (w *Warehouse) updateRobotPos(xInc, yInc int) {
 	(*w.grid)[w.robotPos.y][w.robotPos.x] = '@'
 }
 
-func (w *Warehouse) calcGps() int {
+func (w *Warehouse) calcGps2() int {
+	acc := 0
+	for y := range *w.grid {
+		for x, cell := range (*w.grid)[y] {
+			if cell == '[' {
+				// acc += 100*(len(*w.grid)-y-1) + x
+				acc += 100*y + x
+				// fmt.Printf("O at x=%d, y=%d, their value: %d\n", x, y, 100*(len(*w.grid)-y-1)+x)
+			}
+		}
+	}
+	return acc
+}
+
+func (w *Warehouse) calcGps1() int {
 	acc := 0
 	for y := range *w.grid {
 		for x, cell := range (*w.grid)[y] {
@@ -144,11 +194,14 @@ func (w *Warehouse) update(move byte) {
 			break
 		}
 
-		if crateN, ok := w.canPush(move); ok {
+		if crateN, ok := w.canPushHorizontally(move); ok {
+			fmt.Printf("crateN: %d\n", crateN)
 			// ..[]@
 			// .[]@.
-			(*w.grid)[w.robotPos.y][w.robotPos.x-crateN*2-1] = '['
-			(*w.grid)[w.robotPos.y][w.robotPos.x-crateN*2] = ']'
+			for j := crateN; j > 0; j-- {
+				(*w.grid)[w.robotPos.y][w.robotPos.x-j*2-1] = '['
+				(*w.grid)[w.robotPos.y][w.robotPos.x-j*2] = ']'
+			}
 			// (*w.grid)[w.robotPos.y][w.robotPos.x-crateN-1] = 'O'
 			w.updateRobotPos(-1, 0)
 		}
@@ -163,11 +216,14 @@ func (w *Warehouse) update(move byte) {
 			break
 		}
 
-		if crateN, ok := w.canPush(move); ok {
+		if crateN, ok := w.canPushHorizontally(move); ok {
 			// .@[].
 			// ..@[]
-			(*w.grid)[w.robotPos.y][w.robotPos.x+crateN*2] = '['
-			(*w.grid)[w.robotPos.y][w.robotPos.x+crateN*2+1] = ']'
+
+			for j := crateN; j > 0; j-- {
+				(*w.grid)[w.robotPos.y][w.robotPos.x+j*2+1] = ']'
+				(*w.grid)[w.robotPos.y][w.robotPos.x+j*2] = '['
+			}
 			// (*w.grid)[w.robotPos.y][w.robotPos.x+crateN+1] = 'O'
 			w.updateRobotPos(1, 0)
 		}
@@ -182,8 +238,16 @@ func (w *Warehouse) update(move byte) {
 			break
 		}
 
-		if crateN, ok := w.canPush(move); ok {
-			(*w.grid)[w.robotPos.y-crateN-1][w.robotPos.x] = 'O'
+		if nodes, ok := w.canPushVertically(move); ok {
+			// fmt.Printf("%d\n", len(nodes))
+			for j := len(nodes) - 1; j >= 0; j-- {
+				fmt.Println("we are here")
+				fmt.Printf("shifting cell: %s\n", nodes[j].string())
+				x := nodes[j].x
+				y := nodes[j].y
+				(*w.grid)[y-1][x] = nodes[j].v
+				(*w.grid)[y][x] = '.'
+			}
 			w.updateRobotPos(0, -1)
 		}
 	case 'v':
@@ -196,28 +260,18 @@ func (w *Warehouse) update(move byte) {
 			break
 		}
 
-		if crateN, ok := w.canPush(move); ok {
-			(*w.grid)[w.robotPos.y+crateN+1][w.robotPos.x] = 'O'
+		if nodes, ok := w.canPushVertically(move); ok {
+			for j := len(nodes) - 1; j >= 0; j-- {
+				x := nodes[j].x
+				y := nodes[j].y
+				(*w.grid)[y+1][x] = nodes[j].v
+				(*w.grid)[y][x] = '.'
+			}
 			w.updateRobotPos(0, 1)
 		}
 	default:
 		panic("unrecognized move character\n")
 	}
-}
-
-func (c Coordinate) isAdjacent(co Coordinate) bool {
-	incLayout := [][]int{
-		{0, -1},
-		{1, 0},
-		{0, 1},
-		{-1, 0},
-	}
-	for i := 0; i < len(incLayout); i++ {
-		if c.x == co.x+incLayout[i][0] && c.y == co.y+incLayout[i][1] {
-			return true
-		}
-	}
-	return false
 }
 
 func parseForGrid(line string, lineIdx int, grid *Grid) {
@@ -251,15 +305,15 @@ func parseForMoves(line string, lineIdx int, grid *Grid) {
 	}
 }
 
-func (w *Warehouse) write(moveIdx int) {
-	filename := fmt.Sprintf("%d.txt", moveIdx)
+func (w *Warehouse) write(moveIdx string, move byte) {
+	filename := fmt.Sprintf("%s.txt", moveIdx)
 	f, err := os.Create(filename)
 	if err != nil {
 		panic(err)
 	}
 
 	writer := bufio.NewWriter(f)
-	// fmt.Fprintf(writer, fmt.Sprintf("next move: %s, idx: %d\n", string((*w.moves)[i+1]), i+1))
+	fmt.Fprintf(writer, fmt.Sprintf("next move: %c, idx: %s\n", move, moveIdx))
 	fmt.Fprintf(writer, fmt.Sprintf("current pos: %s\n", w.robotPos.string()))
 	for y, row := range *w.grid {
 		fmt.Fprintf(writer, "%s", string(row))
@@ -273,9 +327,9 @@ func (w *Warehouse) write(moveIdx int) {
 func readLinesFromStream(file *os.File) *Warehouse {
 	scanner := bufio.NewScanner(file)
 	// rows, cols := 8, 8
-	rows, cols := 7, 7
+	// rows, cols := 7, 7
 	// rows, cols := 10, 10
-	// rows, cols := 50, 50
+	rows, cols := 50, 50
 
 	var robotPos Coordinate
 	var grid Grid = make([][]byte, rows)
@@ -294,7 +348,7 @@ func readLinesFromStream(file *os.File) *Warehouse {
 
 		for i, ch := range line {
 			if ch == '@' {
-				robotPos = Coordinate{x: i, y: lineIdx}
+				robotPos = Coordinate{x: i * 2, y: lineIdx}
 			}
 		}
 		// fmt.Printf("len(line)=%d\n", len(line))
